241ef7810891537947255a6cdc557a0eb0bd7679
Yassmine <yasmine.elmoudene2003@gmail.com>
2025-07-04 16:46:39 +0100

    Backend-LangChain

--- services/chat_service.py
+++ services/chat_service.py
@@ -2,13 +2,12 @@ from utils.langchain_client import llm, vector_store
 from langchain.chains import RetrievalQA
 
 async def handle_chat(question: str):
-    
     retriever = vector_store.as_retriever(search_kwargs={"k": 3})
 
     qa_chain = RetrievalQA.from_chain_type(llm=llm, retriever=retriever, chain_type="stuff")
 
-    answer = qa_chain.run(question)
+    answer = qa_chain.invoke({"query": question})
 
-    return {"answer": answer}
+    return {"answer": answer["result"]}
 
 
--- utils/langchain_client.py
+++ utils/langchain_client.py
@@ -1,23 +1,19 @@
 import os
 from dotenv import load_dotenv
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
-from langchain_community.vectorstores import Qdrant
+from langchain_qdrant import Qdrant  # Updated import
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from qdrant_client import QdrantClient
 
 load_dotenv()
 
-
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 QDRANT_URL = os.getenv("QDRANT_URL")
-
 
 embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
 llm = ChatOpenAI(model="gpt-4o", openai_api_key=OPENAI_API_KEY, temperature=0.5)
 
-
 qdrant_client = QdrantClient(url=QDRANT_URL)
 vector_store = Qdrant(client=qdrant_client, collection_name="documents", embeddings=embeddings)
-
 
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
